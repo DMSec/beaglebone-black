51ed6700d81e9dae49772de7f0b808c2afe58399
coo1ovr <external.douglas.costa@pt.bosch.com>
2022-05-12 15:20:06 +0100

    update the drivers and scripts of init and setup wifi

--- scripts/setupWifi-USB-TL-WN722N.sh
+++ scripts/setupWifi-USB-TL-WN722N.sh
@@ -1,25 +1,30 @@
-#!/bin/szh
+#!/bin/bash
 
-szh --version
+echo "Download firmware and install gcc"
+sudo apt-get install firmware-atheros gcc rtl8723de-dkms -y
 
-echo "Update repository apt"
-sudo apt-get update
 
-echo "Installing the wifi libraries"
-sudo apt-get install wireless-tools usbutils
+sudo wget https://static.tp-link.com/TL-WN722N'(US)'_V2_161112_Linux.zip
+sudo mv 'TL-WN722N(US)_V2_161112_Linux.zip' ../drivers/TL-WN722N_V2_161112_Linux.zip
+unzip ../drivers/TL-WN722N_V2_161112_Linux.zip -d ../drivers/TL-WN722N_V2_161112_Linux
 
-echo "Download firmware and install"
-sudo apt-get install firmware-atheros
-sudo wget http://linuxwireless.org/download/htc_fw/1.3/htc_9271.fw
-sudo cp htc_9271.fw /lib/firmware
+#cd ../drivers/TL-WN722N_V2_161112_Linux/rtl8188EUS_linux_v4.3.0.8_13968.20150417
+#ls
+#make
+#make install
+
+cd ../drivers
+git clone https://github.com/morrownr/8812au-20210629.git
+cd 8812au-20210629
+sudo ./install-driver.sh
 
 echo "Backup the original file interface"
-sudo cp /etc/network/interface /etc/network/interface.bkp
+#sudo cp /etc/network/interface /etc/network/interface.bkp
 echo "Copy the custom interface file to /etc/network/interface"
-sudo cp config/interface /etc/network/interface
+#sudo cp config/interface /etc/network/interface
 
-sudo iwconfig
+#sudo iwconfig
 
-sudo connmanctl disable wifi
-sudo connmanctl enable wifi
-sudo ifup wlan0
+#sudo connmanctl disable wifi
+#sudo connmanctl enable wifi
+#sudo ifup wlan0
