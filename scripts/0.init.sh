876ba50e2ad77c5cbdbb14b4831dcc8b3d8d68ca
douglasmsi <douglasmsi@gmail.com>
2022-06-18 10:56:55 +0100

    update username

--- scripts/0.init.sh
+++ scripts/0.init.sh
@@ -3,7 +3,7 @@
 echo "Update repository apt"
 apt update
 apt upgrade -y
-apt instal-y l linux-headers-$(uname -r) build-essential dkms git libelf-dev python-software-properties software-properties-common wireless-tools usbutils isc-dhcp-server hostapd
+apt install -y linux-headers-$(uname -r) build-essential dkms git libelf-dev software-properties-common wireless-tools usbutils isc-dhcp-server hostapd
 
 
 
